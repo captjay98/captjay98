a61bc027a50b560dc535d1acab8b1d8202abc0f9
captjay98 <54381061+captjay98@users.noreply.github.com>
2022-02-09 00:19:28 +0100

    moving me to my readme

---
+++ MeObject.py
@@ -0,0 +1,34 @@
+class Human:
+
+    def __init__(self, name, age, occupation, height, weight, complexion):
+        self.name = name
+        self.occupation = occupation
+        self.age = age
+        self.height = height
+        self.weight = weight
+        self.complexion = complexion
+
+    def likes(self):
+        if self.name == "Jamal":
+            print(self.name, "Plays Xbox games alot")
+        elif self.name == "random":
+            print(self.name, "does what humans do")
+
+    def speaks(self):
+        print(self.name, "Speaks English")
+
+    def loves(self):
+        if self.occupation == "Programmer":
+            print(self.name, "Loves Python")
+        elif self.occupation == "Graduate":
+            print(self.name, "Just doing regular work")
+
+
+me = Human("Jamal", 23, "Programmer", 180, 65, 'dark')
+me.likes()
+me.loves()
+me.speaks()
+
+print(f"Jamal is {me.age} years old")
+print(f"He is {me.height}CM Tall")
+print(f"And weighs {me.weight}KG")
