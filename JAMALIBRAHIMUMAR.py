68faf4dce5004bc6c13a855ce32972ff0ca963d4
captjay98 <captjay98@gmail.com>
2022-06-17 04:56:50 +0100

    Done with this

--- JAMALIBRAHIMUMAR.py
+++ JAMALIBRAHIMUMAR.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 print("Hey, Jamal here or not here😂'\n")
 
 print("This is just a simple Bot that can tell you some things about me.")
@@ -12,11 +13,13 @@ print("It can also tell you some of my hobbies.\n")
 aboutMe = {"my_name": "JAMAL IBRAHIM UMAR",
            "my_age": 2021 - 1998,
            "my_height": 183,
+           "my_weight": 67,
            "my_eyes": "BROWN",
            "my_teeth": "WHITE",
            "my_hair": "BLACK",
            "my_qualification": "Python Dev",
            }
+
 print("p.s Just input a word corresponding to what you want to know")
 print("Say you want to know my weight, just type weight")
 print("My hobbies?, Just type hobbies")
@@ -27,22 +30,22 @@ while True:
     try:
       Q1 = input("Let's go, What would you like to know?: ")
 
-      if Q1 == "What's your name?":
-          print(f"My name is {aboutMe[5]}.")
+      if Q1 == "Name":
+          print(f'My name is {aboutMe["my_name"]}.')
 
-      elif Q1 == "How old are you?":
-          print(f"I am {my_age} years old")
+      elif Q1 == "age":
+          print(f'I am {aboutMe["my_age"]} years old')
 
-      elif Q1 == "How about your height?":
-          print(f"I am {my_height} CM tall")
+      elif Q1 == "height":
+          print(f'I am {aboutMe["my_height"]} CM tall')
 
-      elif Q1 == "And your weight?":
-          print(f"I am {my_weight} KG heavy")
+      elif Q1 == "weight":
+          print(f'I am {aboutMe["my_weight"]} KG heavy')
           print("I know I'm not that heavy")
 
-      elif Q1 == "Any other thing?":
-          print(f"I have {my_eyes} eyes and my hair is {my_hair}")
-          print(f"My teeth are usually {my_teeth} .")
+      elif Q1 == "others":
+          print(f'I have {aboutMe["my_eyes"]} eyes and my hair is {aboutMe["my_hair"]}')
+          print(f'My teeth are usually {aboutMe["my_teeth"]}.')
           print('And i Absolutely Love python')
 
       else:
